900ba16283b7a7a631a58f713e5c0b5d9ce3c740
Wissam Fawaz <wissamfawaz12@gmail.com>
2023-01-22 17:39:30 +0200

    Updated the script fetching the quotes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 getQuote();
-//fetching the quotes from type.fit/api/quotes and picking one at random 
+//fetching the quotes from type.fit/api/quotes and picking one at random
 function getQuote() {
-  fetch('https://type.fit/api/quotes')
-  .then(response => response.text())
-  .then(data => {
-    const quotes = JSON.parse(data);
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const quote = quotes[randomIndex];
-    displayQuote(quote.text, quote.author);
-  });
+  fetch("https://type.fit/api/quotes")
+    .then((response) => response.text())
+    .then((data) => {
+      const quotes = JSON.parse(data);
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const quote = quotes[randomIndex];
+      if (quote !== null) {
+        const quoteAuthor = quote.author ? quote.author : "Unknown Author";
+        const quoteText = quote.text;
+        displayQuote(quoteText, quoteAuthor);
+      }
+    });
 }
 //displaying the quote and the author
-function displayQuote(quote, author) {
-  if (quote !== null) {
+function displayQuote(quoteText, quoteAuthor) {
+  if (quoteText !== null) {
     const textParagraph = document.querySelector(".quote");
-    textParagraph.innerHTML = `"${quote}" &mdash; <strong>${author}</strong>`;
+    textParagraph.innerHTML = `"${quoteText}" &mdash; <strong>${quoteAuthor}</strong>`;
   }
 }
